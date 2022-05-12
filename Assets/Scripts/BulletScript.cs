9722a908ed9a567ccbaa84550d4b2f67bf51f03a
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-12 17:31:24 +0530

    no message

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	#region PUBLIC VARIABLES
+	// The bullet's speed in Unity units.
+	public float speed = 7f;
+	#endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	#region PRIVATE VARIABLES
+	private Camera mainCamera;
+	#endregion
+
+	#region MONOBEHAVIOUR METHODS
+	void Start()
+	{
+		mainCamera = Camera.main;
+	}
+
+	void Update()
+	{
+		Vector3 newPosition = transform.position + transform.forward * speed * Time.deltaTime;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+
+		//Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+		//if (viewportPosition.x > 1 || viewportPosition.x < 0 || viewportPosition.y > 1 || viewportPosition.y < 0)
+		//{
+		//	PoolManager.Instance.Recycle(Constants.BULLET_PREFAB_NAME, gameObject);
+		//}
+	}
+	#endregion
+
+	#region PUBLIC METHODS
+	// Set the position of the bullet.
+	public void SetPosition(Vector3 position)
+	{
+		transform.position = position;
+	}
+
+	// Set the tragectory of the bullet.
+	public void SetTrajectory(Vector3 target)
+	{
+		transform.LookAt(target, Vector3.back);
+	}
+	#endregion
+
+
 }
